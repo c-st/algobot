f5d1ca6cb85d2669c8efc91360baa0103102b517
Christian Stangier <christian@stangier.email>
2021-04-02 23:19:22 +0200

    Add a simple endpoint handled by a Lambda

--- lib/algobot-stack.ts
+++ lib/algobot-stack.ts
@@ -1,4 +1,15 @@
+import * as path from "path";
 import * as CDK from "@aws-cdk/core";
+import * as ApiGateway from "@aws-cdk/aws-apigateway";
+import * as LambdaNodeJs from "@aws-cdk/aws-lambda-nodejs";
+import * as Lambda from "@aws-cdk/aws-lambda";
+
+const DEFAULT_LAMBDA_SETTINGS = {
+  timeout: CDK.Duration.seconds(5),
+  memorySize: 128,
+  tracing: Lambda.Tracing.ACTIVE,
+  runtime: Lambda.Runtime.NODEJS_14_X,
+};
 
 export class AlgobotStack extends CDK.Stack {
   urlOutput: CDK.CfnOutput;
@@ -6,10 +17,23 @@ export class AlgobotStack extends CDK.Stack {
   constructor(scope: CDK.Construct, id: string, props?: CDK.StackProps) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
+    const helloWorldHandler = new LambdaNodeJs.NodejsFunction(
+      this,
+      "HelloWorldHandler",
+      {
+        functionName: "HelloWorld",
+        entry: path.join(__dirname, "../src/helloWorldHandler.ts"),
+        ...DEFAULT_LAMBDA_SETTINGS,
+      }
+    );
+
+    const api = new ApiGateway.LambdaRestApi(this, "Gateway", {
+      description: "Endpoint for a simple Lambda-powered web service",
+      handler: helloWorldHandler,
+    });
 
     this.urlOutput = new CDK.CfnOutput(this, "url", {
-      value: "TODO replace with api.url",
+      value: api.url,
     });
   }
 }
---
+++ src/helloWorldHandler.test.ts
@@ -0,0 +1,5 @@
+describe("HelloWorld handler", () => {
+  it("true is true", () => {
+    expect(true).toBeTruthy();
+  });
+});
