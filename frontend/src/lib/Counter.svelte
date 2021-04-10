dd1ec751032fbed9be6c7e42129c12f2a4706b5d
Christian Stangier <christian@stangier.email>
2021-04-10 22:14:45 +0200

    Add Tailwindcss

--- frontend/src/lib/Counter.svelte
+++ frontend/src/lib/Counter.svelte
@@ -5,7 +5,7 @@
   }
 </script>
 
-<button on:click={increment}>
+<button on:click={increment} class="bg-gray-200">
   Clicks: {count}
 </button>
 
@@ -13,22 +13,10 @@
   button {
     font-family: inherit;
     font-size: inherit;
-    padding: 1em 2em;
-    color: #ff3e00;
-    background-color: rgba(255, 62, 0, 0.1);
+    padding: 0.5em 1em;
     border-radius: 2em;
-    border: 2px solid rgba(255, 62, 0, 0);
     outline: none;
-    width: 200px;
     font-variant-numeric: tabular-nums;
     cursor: pointer;
-  }
-
-  button:focus {
-    border: 2px solid #ff3e00;
-  }
-
-  button:active {
-    background-color: rgba(255, 62, 0, 0.2);
   }
 </style>
---
+++ frontend/src/lib/HelloWorld.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+  import Counter from "./Counter.svelte";
+</script>
+
+<main class="flex h-screen">
+  <div class="m-auto bg-gray-100 rounded-xl p-6">
+    <h1 class="text-blue text-lg font-semibold">Hello World</h1>
+    <Counter />
+  </div>
+</main>
+
+<style>
+</style>
